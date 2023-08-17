dab034a9a7e50a44b40d01b03aa277e13c55e396
valentin03postigo <valentinpostigo03@gmail.com>
2023-08-17 16:18:04 -0300

    final

--- script.js
+++ script.js
@@ -1,27 +1,83 @@
-const seconds = document.getElementById('seconds');
-const miliseconds = document.getElementById('ms');
+const seconds_counter = document.getElementById('seconds');
+const miliseconds_counter = document.getElementById('ms');
 
 const start = document.getElementById('start-btn');
 const stop = document.getElementById('stop-btn');
 const reset = document.getElementById('reset-btn');
 
+let seconds = 0;
+let miliseconds = 0;
 
+let increment = function () {
 
-start.addEventListener('click', function(){
+    miliseconds += 1;
 
-    function ms(){
-        miliseconds.textContent = new Date().getMilliseconds();
+    if ( miliseconds == 99 ) {
+
+        miliseconds = 0;
+        seconds += 1;
+
+        if (miliseconds < 10 ){
+
+            miliseconds_counter.textContent = "0" + miliseconds;
+
+        } else {
+
+            miliseconds_counter.textContent = miliseconds;
+
+        }
+
+        if ( seconds < 10 ) {
+
+            seconds_counter.textContent = "0" + seconds;
+
+        } else {
+
+            seconds_counter.textContent = seconds;
+
+        }
+
+    } else {
+
+        if (miliseconds < 10 ){
+
+            miliseconds_counter.textContent = "0" + miliseconds;
+
+        } else {
+
+            miliseconds_counter.textContent = miliseconds;
+
+        }
+
     }
-    setInterval(ms, 1)
 
-    function s(){
-        seconds.textContent = new Date().getSeconds();
-    }
+}
 
-    setInterval(s, 1000)
+let incrementInterval 
+
+start.addEventListener( 'click', function (){
+
+    incrementInterval = setInterval(() => {
+
+        increment()
+
+    }, 1)
 
 })
 
-stop.addEventListener('click', function(){
-    
+stop.addEventListener( 'click', function(){
+
+    clearInterval(incrementInterval)
+
+})
+
+reset.addEventListener( 'click' , function () {
+
+    seconds = 0;
+    miliseconds = 0;
+
+    miliseconds_counter.textContent = "00"
+
+    seconds_counter.textContent = "00"
+
 })
